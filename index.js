87b3f03141df9c0ad3432ffa55e0c2d7f5c957ab
erik santiago <blueowl2794@gmail.com>
2022-07-27 12:59:32 -0500

    modified(index.js)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended:true
 }));
-server.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
