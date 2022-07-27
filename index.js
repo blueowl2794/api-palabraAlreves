8c6c8700ef1a9d098f264d6607761a39d537f4ba
erik santiago <blueowl2794@gmail.com>
2022-07-27 12:46:03 -0500

    add(access-origin)

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended:true
 }));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 app.get('/iecho', palabraAlreves);//iecho?text=test
 
